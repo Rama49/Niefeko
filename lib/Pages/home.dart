e2f4340dfe7d1fc5fe5dfe52e30747607a947752
rama.fall <ramafall49@gmail.com>
2024-04-23 12:55:01 +0000

    pusher la page inscription

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:niefeko/Components/Carte/carte.dart';
-import 'package:niefeko/Components/Carte/recherche/recherche.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:niefeko/Pages/Inscription/inscription.dart';
 
 class MyHomePage extends StatefulWidget {
