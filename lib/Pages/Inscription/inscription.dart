dba39e9f4cb2393d997053cd9abaaafe3b11c89a
rama.fall <ramafall49@gmail.com>
2024-04-23 18:18:44 +0000

    logique conexion deja pris

--- lib/Pages/Inscription/inscription.dart
+++ lib/Pages/Inscription/inscription.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:niefeko/Components/Carte/recherche/recherche.dart';
 import 'package:niefeko/Pages/Connexion/conexion.dart';
 import 'package:niefeko/Reutilisable/buttonReu.dart';
@@ -63,10 +66,17 @@ class _inscriptionState extends State<inscription> {
                   child: Column(children: [
                     Padding(
                       padding:
-                          const EdgeInsets.only(top:10, left: 40, right: 40),
+                          const EdgeInsets.only(top: 10, left: 40, right: 40),
                       child: TextFormField(
-                        style: TextStyle(
-                            color: Colors.white), // Couleur du texte
+                        style:
+                            TextStyle(color: Colors.white), // Couleur du texte
+                            controller: _nomController,
+                              validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Veuillez entrer votre nom';
+                      }
+                      return null;
+                    },
                         decoration: InputDecoration(
                           labelText: 'Nom',
                           labelStyle: TextStyle(
@@ -93,6 +103,13 @@ class _inscriptionState extends State<inscription> {
                       child: TextFormField(
                         style:
                             TextStyle(color: Colors.white), // Couleur du texte
+                            controller: _prenomController,
+                             validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Veuillez entrer votre prénom';
+                      }
+                      return null;
+                    },
                         decoration: InputDecoration(
                           labelText: 'Prenom',
                           labelStyle: TextStyle(
@@ -119,6 +136,13 @@ class _inscriptionState extends State<inscription> {
                       child: TextFormField(
                         style:
                             TextStyle(color: Colors.white), // Couleur du texte
+                             controller: _emailController,
+                                validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Veuillez entrer votre email';
+                      }
+                      return null;
+                    },
                         decoration: InputDecoration(
                           labelText: 'Email',
                           labelStyle: TextStyle(
@@ -145,6 +169,15 @@ class _inscriptionState extends State<inscription> {
                       child: TextFormField(
                         style:
                             TextStyle(color: Colors.white), // Couleur du texte
+                            controller: _passwordController,
+                            
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Veuillez entrer votre mot de passe';
+                      }
+                      return null;
+                    },
                         decoration: InputDecoration(
                           labelText: 'Mot de passe',
                           labelStyle: TextStyle(
@@ -171,6 +204,14 @@ class _inscriptionState extends State<inscription> {
                       child: TextFormField(
                         style:
                             TextStyle(color: Colors.white), // Couleur du texte
+                             controller: _confirmPasswordController,
+                              obscureText: true,
+                    validator: (value) {
+                      if (value != _passwordController.text) {
+                        return 'Les mots de passe ne correspondent pas';
+                      }
+                      return null;
+                    },
                         decoration: InputDecoration(
                           labelText: 'Confirmer mot de passe',
                           labelStyle: TextStyle(
@@ -191,44 +232,84 @@ class _inscriptionState extends State<inscription> {
                       ),
                     ),
                     SizedBox(height: 20),
-                                        BoutonR(
-                      titre: "S'inscrire",
+                    BoutonR(
+                    titre: "S'inscrire",
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        try {
+                          UserCredential userCredential = await FirebaseAuth
+                              .instance
+                              .createUserWithEmailAndPassword(
+                            email: _emailController.text,
+                            password: _passwordController.text,
+                          );
+
+                          await FirebaseFirestore.instance
+                              .collection('Inscription')
+                              .doc(userCredential.user!.uid)
+                              .set({
+                            'nom': _nomController.text,
+                            'prenom': _prenomController.text,
+                            'email': _emailController.text,
+                            'password': _passwordController.text,
+                            'confirmPassword': _confirmPasswordController.text,
+                          });
+
+                          Fluttertoast.showToast(
+                            msg: "Inscription réussie avec succès",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 3, // Durée d'affichage de 3 secondes
+                            backgroundColor: Colors.white,
+                            textColor: Colors.purple,
+                            fontSize: 16.0,
+                          );
+
+                          // Vider tous les champs du formulaire après l'inscription réussie
+                          _nomController.clear();
+                          _prenomController.clear();
+                          _emailController.clear();
+                          _passwordController.clear();
+                          _confirmPasswordController.clear();
+                        } catch (e) {
+                          print('Erreur lors de l\'inscription : $e');
+                         }
+                      }
+                    },
+                  ),
+                    ElevatedButton(
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => conexion()),
+                          MaterialPageRoute(builder: (context) => search()),
                         );
                       },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF612C7D),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ),
+                      child: Text(
+                        "Avez vous deja un compte? Se connecter",
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
                     ),
-                     ElevatedButton(
-       onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => search()),
-                      );
-                    },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Color(0xFF612C7D),
-        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-        shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(5)
-        ),
-      ),
-      child: Text(
-        "Avez vous deja un compte? Se connecter",
-        style: TextStyle(
-          fontSize: 
-              14,
-          color: Colors.white
-        ),
-      ),
-    ),
-
                   ]),
                 ),
               ],
             )),
       ),
     );
+  }
+   @override
+  void dispose() {
+    _nomController.dispose();
+    _prenomController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
   }
 }
