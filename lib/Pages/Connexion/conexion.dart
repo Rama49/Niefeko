f0ecbfd784c0815b96a6b5988bbca53c65eca45b
Ndeye Sokhna Gueye <touty1703@gmail.com>
2024-04-23 09:22:38 +0000

    first commit

--- lib/Pages/Connexion/conexion.dart
+++ lib/Pages/Connexion/conexion.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class conexion extends StatefulWidget {
+  const conexion({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<conexion> createState() => _conexionState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _conexionState extends State<conexion> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
