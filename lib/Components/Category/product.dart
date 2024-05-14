dc722bd2b9873f217f07c269c0ce4181c62394b2
MohamedLoum1001 <mloum137@gmail.com>
2024-05-14 12:24:18 +0000

    test historique

--- lib/Components/Category/product.dart
+++ lib/Components/Category/product.dart
@@ -1,32 +1,32 @@
-//import 'package:flutter/material.dart';
+// //import 'package:flutter/material.dart';
 
-class Product {
-  //final int id; 
-  final String imagePath, name, description;
-  final double price;
-  int quantity;
+// class Product {
+//   //final int id; 
+//   final String imagePath, name, description;
+//   final double price;
+//   int quantity;
 
-  // Product( {
-  //   //  required this.id,
-  //     required this.imagePath,
-  //     required this.name,
-  //     required this.description,
-  //     required this.price,
-  //     });
-       Map<String, dynamic> toMap() {
-    return {
-      'imagePath': imagePath,
-      'name': name,
-      'desciption': description,
-      'price': price,
-    };
-    }
+//   // Product( {
+//   //   //  required this.id,
+//   //     required this.imagePath,
+//   //     required this.name,
+//   //     required this.description,
+//   //     required this.price,
+//   //     });
+//        Map<String, dynamic> toMap() {
+//     return {
+//       'imagePath': imagePath,
+//       'name': name,
+//       'desciption': description,
+//       'price': price,
+//     };
+//     }
 
-  Product({
-      required this.imagePath,
-      required this.name,
-      required this.price,
-      required this.description,
-      this.quantity = 1,
-    });
-}
+//   Product({
+//       required this.imagePath,
+//       required this.name,
+//       required this.price,
+//       required this.description,
+//       this.quantity = 1,
+//     });
+// }
