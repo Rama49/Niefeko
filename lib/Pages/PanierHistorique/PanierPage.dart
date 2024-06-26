c76b12ac872dcf07c1f942a5d9be00d3951f10b0
rama.fall <ramafall49@gmail.com>
2024-06-26 13:06:28 +0000

    recuperer les details commandes

--- lib/Pages/PanierHistorique/PanierPage.dart
+++ lib/Pages/PanierHistorique/PanierPage.dart
@@ -21,7 +21,6 @@ class Order {
   });
 
   factory Order.fromJson(Map<String, dynamic> json) {
-    // Parsing the 'line_items' field from JSON
     var itemsJson = json['line_items'] as List;
     List<OrderItem> itemsList =
         itemsJson.map((item) => OrderItem.fromJson(item)).toList();
@@ -42,12 +41,16 @@ class OrderItem {
   final String productName;
   final double price;
   final int quantity;
+  final String productImage;
+  final String dateCreated; // Ajout de la date de création
 
   OrderItem({
     required this.productId,
     required this.productName,
     required this.price,
     required this.quantity,
+    required this.productImage,
+    required this.dateCreated,
   });
 
   factory OrderItem.fromJson(Map<String, dynamic> json) {
@@ -56,6 +59,9 @@ class OrderItem {
       productName: json['name'],
       price: double.parse(json['price'].toString()),
       quantity: int.parse(json['quantity'].toString()),
+      productImage: json['imagePath'],
+      dateCreated: json[
+          'date_created'], // Assurez-vous que 'date_created' correspond à la clé JSON
     );
   }
 }
@@ -76,24 +82,24 @@ class _PanierPageState extends State<PanierPage> {
   }
 
   Future<void> fetchOrders() async {
-    final url =
-        Uri.parse('https://niefeko.com/wp-json/custom-routes/v1/customer/orders');
+    final url = Uri.parse(
+        'https://niefeko.com/wp-json/custom-routes/v1/customer/orders');
 
     try {
       final prefs = await SharedPreferences.getInstance();
       final token = prefs.getString('token') ?? '';
-      print('Token: $token'); // Print the token for debugging
+      print('Token: $token');
 
       final response = await http.get(
         url,
         headers: {'Authorization': 'Bearer $token'},
       );
-      print('Response status: ${response.statusCode}'); // Print status code
-      print('Response body: ${response.body}'); // Print raw response body
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
 
       if (response.statusCode == 200) {
         final List<dynamic> responseData = json.decode(response.body);
-        print('Response data: $responseData'); // Print the response data
+        print('Response data: $responseData');
 
         setState(() {
           orders = responseData.map((json) => Order.fromJson(json)).toList();
@@ -127,6 +133,119 @@ class _PanierPageState extends State<PanierPage> {
     }
   }
 
+Future<void> fetchOrderDetails(int orderId) async {
+  final url = Uri.parse(
+      'https://niefeko.com/wp-json/custom-routes/v1/customer/order/$orderId');
+  try {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token') ?? '';
+
+    final response = await http.get(
+      url,
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+
+      print('Response Data: $responseData'); // Ajout pour débogage
+
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Détails de la Commande ${responseData['order_id']}'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Statut: ${responseData['status']}'),
+                Text('Email: ${responseData['email']}'), // Assurez-vous de la clé exacte
+                Text(
+                    'Total: ${responseData['total']} ${responseData['currency']}'),
+                Divider(),
+                Text('Articles:'),
+                ...(responseData['line_items'] as List).map((item) {
+                  return ListTile(
+                    leading: Image.network(item['productImage']),
+                    title: Text(item['productName']),
+                    subtitle: Text(
+                        'Prix: ${item['price']} ${responseData['currency']}\nQuantité: ${item['quantity']}\nDate: ${item['date_created']}'),
+                  );
+                }).toList(),
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Fermer'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      throw Exception('Failed to load order details: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error fetching order details: $e');
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Failed to fetch order details: $e'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+  Future<void> deleteOrder(int orderId) async {
+    final url = Uri.parse(
+        'https://niefeko.com/wp-json/custom-routes/v1/customer/orders/$orderId');
+
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('token') ?? '';
+
+      final response = await http.delete(
+        url,
+        headers: {'Authorization': 'Bearer $token'},
+      );
+
+      print('Delete response status: ${response.statusCode}');
+      print('Delete response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        setState(() {
+          orders.removeWhere((order) => order.orderId == orderId);
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Commande supprimée avec succès')),
+        );
+      } else {
+        throw Exception('Failed to delete order: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error deleting order: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text('Erreur lors de la suppression de la commande: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -148,6 +267,9 @@ class _PanierPageState extends State<PanierPage> {
                     final order = orders[index];
                     return Card(
                       child: ListTile(
+                        leading: order.lineItems.isNotEmpty
+                            ? Image.network(order.lineItems[0].productImage)
+                            : null,
                         title: Text('Commande ${order.orderId}'),
                         subtitle: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -156,36 +278,14 @@ class _PanierPageState extends State<PanierPage> {
                             Text('Statut: ${order.status}'),
                           ],
                         ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            deleteOrder(order.orderId);
+                          },
+                        ),
                         onTap: () {
-                          // Show order details
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text('Détails de la Commande'),
-                                content: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: order.lineItems.map((item) {
-                                    return ListTile(
-                                      title: Text(item.productName),
-                                      subtitle: Text(
-                                        'Prix: ${item.price} ${order.currency}\nQuantité: ${item.quantity}',
-                                      ),
-                                    );
-                                  }).toList(),
-                                ),
-                                actions: <Widget>[
-                                  TextButton(
-                                    child: Text('Fermer'),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                ],
-                              );
-                            },
-                          );
+                          fetchOrderDetails(order.orderId);
                         },
                       ),
                     );
@@ -194,4 +294,3 @@ class _PanierPageState extends State<PanierPage> {
     );
   }
 }
-
